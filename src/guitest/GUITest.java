a1a5f32a96bb00bd77d3ffebfbb584b7be5fb22d
ffuis-aljosa <Korisnik@FF-206-MAIN-W7>
2015-03-10 09:28:18 +0100

    Initial commit

---
+++ src/guitest/GUITest.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package guitest;
+
+/**
+ *
+ * @author Korisnik
+ */
+public class GUITest {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Form mainForm = new Form();
+        mainForm.setVisible(true);
+    }
+    
+}
